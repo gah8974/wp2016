8f74859ea675032bec10d23b62877e9c6c941386
Chris Xiao <gah8974@gmail.com>
2016-10-19 23:59:43 +0800

    cg app.js and new pic

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready( function() {
   $('#Myphoto').mouseover(function() {
-    $('#Myphoto').attr('src','pic/Background2.jpg');
+    $('#Myphoto').attr('src','pic/1010.jpg');
   });
   
   $('#Myphoto').mouseout(function() {
